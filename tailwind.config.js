1b9efba1f952d07140fb245069006b1bf1231356
Afaz Khatri <afaz.khatri@bacancy.com>
2024-03-21 14:19:15 +0530

    Completed the blog component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,vue}",
   ],
+  prefix: 'bc-',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: ["Madimi One", 'sans-serif', ...defaultTheme.fontFamily.sans],
+        sans: ["Quicksand", 'sans-serif', ...defaultTheme.fontFamily.sans]
+      }
+    },
   },
   plugins: [],
 }
